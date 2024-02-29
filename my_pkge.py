dc9043a337726918b869e82c26de26f4ea42b975
Lachlan J. Rogers <lachlan.j.rogers@quantum.diamonds>
2024-02-29 22:32:11 +1100

    Getting started on tutorial content

---
+++ my_pkge.py
@@ -0,0 +1,15 @@
+def get_favourite_subject(person_name):
+    """Get the favourite university subject for a chosen person
+    
+    Params
+    ------
+    person_name : string
+        Name of the desired person
+        
+    Returns
+    -------
+    favourite_subject : string
+        Name of the favourite university subject
+    """
+        
+    return 'physics'
